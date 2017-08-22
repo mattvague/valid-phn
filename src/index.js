b22be29693d9b658b038c2370e822d72254a1f2f
Matt Vague <mattvague@gmail.com>
2017-08-22 19:10:59 -0400

    Add comments and refactor

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-const weights = [2, 4, 8, 5, 10, 9, 7, 3]
+// Validator for Canadian PHNs (Personal Health Numbers)
+// http://www2.gov.bc.ca/assets/gov/health/practitioner-pro/software-development-guidelines/app_d.pdf
+
+const DIGIT_WEIGHTS = [2, 4, 8, 5, 10, 9, 7, 3]
 
 export const validPhn = (phn) => {
   const phnInt = parseInt(phn)
 
+  // PHN must be an integer or it ain't valid
   if (isNaN(phnInt)) { return false }
 
+  // Split PHN into array of integer
   const phnDigits = Array.from(phnInt.toString()).map(Number)
+
+  // Shift & pop first and last digits
   const firstDigit = phnDigits.shift()
   const lastDigit = phnDigits.pop()
 
+  // PHN must start with 9 or it ain't valid
   if (firstDigit !== 9) { return false }
 
+  // Multiply each digit by the specified digit weight, divide each by
+  // 11, and grab the remainder. Finally, add all of the digits up, divide
+  // by 11 and grab the total remainder
   const digitRemainderTotal = phnDigits
-    .map((digit, i) => (digit * weights[i]) % 11)
+    .map((digit, i) => (digit * DIGIT_WEIGHTS[i]) % 11)
     .reduce((m, i) => m + i) % 11
 
+  // Total remainder subtracted from 11 must equal last digit to be valid
   return (11 - digitRemainderTotal) === lastDigit
 }
